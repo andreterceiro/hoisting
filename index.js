ac3a8c1381b590bc726f72a8bf79404b628eaf73
André de Paula Terceiro <andre@terceiro.com.br>
2023-09-01 08:14:16 -0300

    End of the second part of the study

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-"use strict"; 
+//"use strict"; 
 // A linha acima sem o comentário forçará um erro já no console.log do b
-var a = 1;
+var a;
 
 function soma() {
     var b = 1;
@@ -19,3 +19,14 @@ console.log(typeof c);
 // sendo declarada depois de ser usada
 var b = 0;
 var c = 0;
+
+const d = "constante";
+a = 3;
+// A linha abaixo dá erro independente do escopo
+// Não se pode reatribuir um valor para uma constante
+//d = 4;
+
+// E não se pode declarar uma constante sem se atribuir um valor
+// para ela. Logo, a linha abaixo também dará erro se descomentada
+// Isto independente do "use strict"; ou não 
+// const e;
